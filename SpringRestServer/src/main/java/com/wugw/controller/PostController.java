47826ae91542b37d2e29c5c408268c7a3e7c75f3
GenweiWu <wugenwei2010@163.com>
2017-03-13 21:33:39 +0800

    server端： 演示post方式的参数传递

---
+++ SpringRestServer/src/main/java/com/wugw/controller/PostController.java
@@ -0,0 +1,62 @@
+package com.wugw.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 针对post方法的参数传递
+ */
+@RestController
+public class PostController {
+
+    /**
+     * 测试参数String
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/book", method = {RequestMethod.POST})
+    public String getBook(@RequestParam("id") String id) {
+        return "book >>> " + id;
+    }
+
+    /**
+     * 测试参数List
+     *
+     * @param ids
+     * @return
+     */
+    @RequestMapping(value = "/books", method = {RequestMethod.POST})
+    public List<String> getBooks(@RequestParam("ids") List<String> ids) {
+        return ids.stream().map(u -> "book >>> " + u).collect(Collectors.toList());
+    }
+
+    /**
+     * 测试参数ObjectNode
+     *
+     * @param ids
+     * @return
+     */
+    @RequestMapping(value = "/bookInfo", method = {RequestMethod.POST})
+    public List<ObjectNode> getBookInfo(@RequestParam("ids") List<String> ids) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        List<ObjectNode> bookInfo = new ArrayList<>();
+        ids.forEach(b -> {
+            ObjectNode node = mapper.createObjectNode();
+            node.put("id", b);
+            node.put("book", String.format("<<%s>>", b));
+
+            bookInfo.add(node);
+        });
+        return bookInfo;
+    }
+}
